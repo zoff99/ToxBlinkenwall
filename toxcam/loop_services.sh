0ba80ecd42a997b78267fbfcf8bb5e37d97c7fa7
Zoff <zoff99@users.noreply.github.com>
2017-06-11 19:17:37 +0200

    script updates

--- toxcam/loop_services.sh
+++ toxcam/loop_services.sh
@@ -10,7 +10,7 @@ video_device=$(ls -1 /dev/video*|tail -1)
 cd $(dirname "$0")
 
 while [ 1 == 1 ]; do
-	./toxcam_static -d $video_device # > /dev/null 2> /dev/null
-	sleep 4
+	./toxcam_static -d $video_device -t # > /dev/null 2> /dev/null
+	sleep 10
 done
 
---
+++ toxcam/update_from_ci.sh
@@ -0,0 +1,13 @@
+﻿#! /bin/bash
+
+#####################################################
+# update toxcam binary from Circle CI (master branch)
+#####################################################
+
+cd $(dirname "$0")
+
+pkill toxcam_static # will stop toxcam
+cp -av toxcam_static toxcam_static__BACKUP
+wget -O toxcam_static 'https://circleci.com/api/v1/project/zoff99/ToxCam/latest/artifacts/0/$CIRCLE_ARTIFACTS/RASPI/toxcam_static?filter=successful&branch=master'
+chmod u+rwx toxcam_static
+
