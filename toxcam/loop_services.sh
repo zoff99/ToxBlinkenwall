5b3ea38fd8141e55652a0e757376bb2c55673234
Zoff <zoff99@users.noreply.github.com>
2017-06-12 13:26:52 +0200

    tweak

--- toxcam/loop_services.sh
+++ toxcam/loop_services.sh
@@ -10,7 +10,7 @@ video_device=$(ls -1 /dev/video*|tail -1)
 cd $(dirname "$0")
 
 while [ 1 == 1 ]; do
-	./toxcam_static -d $video_device -t # > /dev/null 2> /dev/null
+	./toxcam_static -d $video_device -2 # > /dev/null 2> /dev/null
 	sleep 10
 done
 
