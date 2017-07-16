17f30a3ecc6c140c08b1bab9ae980d3cda5b1e88
zoff99 <dummy@vps263487.ovh.net>
2017-07-16 20:24:40 +0200

    renanem stuff

--- toxblinkenwall/loop_services.sh
+++ toxblinkenwall/loop_services.sh
@@ -1,16 +1,9 @@
 #! /bin/bash
 
-#####################################################
-# pick first available video device
-# change for your needs here!
-video_device=$(ls -1 /dev/video*|tail -1)
-#
-#####################################################
-
 cd $(dirname "$0")
 
 while [ 1 == 1 ]; do
-	./toxcam_static -d $video_device -2 # > /dev/null 2> /dev/null
+	./toxblinkenwall
 	sleep 10
 done
 
