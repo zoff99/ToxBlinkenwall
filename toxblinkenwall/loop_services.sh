3739b9f994fddef21892be96b338c2cf217d45b0
Zoff <zoff99@users.noreply.github.com>
2017-07-19 14:53:08 +0200

    Update loop_services.sh

--- toxblinkenwall/loop_services.sh
+++ toxblinkenwall/loop_services.sh
@@ -3,6 +3,7 @@
 function clean_up {
 
 	# Perform program exit cleanup of framebuffer
+	scripts/stop_loading_endless.sh
 	scripts/cleanup_fb.sh
 	exit
 }
@@ -11,14 +12,16 @@ cd $(dirname "$0")
 export LD_LIBRARY_PATH=usr/lib
 
 sudo /etc/init.d/lightdm start
-sleep 10
+sleep 4
 
 trap clean_up SIGHUP SIGINT SIGTERM
+scripts/stop_loading_endless.sh
 scripts/init.sh
 sleep 2
 scripts/create_gfx.sh
 
 while [ 1 == 1 ]; do
+	scripts/stop_loading_endless.sh
 	scripts/init.sh
 	./toxblinkenwall
 	sleep 10
