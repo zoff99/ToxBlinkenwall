51e8e611f430218d7af38bade5b0a496835747a5
Zoff <zoff99@users.noreply.github.com>
2017-07-21 16:11:12 +0200

    Create vars.sh

---
+++ toxblinkenwall/scripts/vars.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+##### -------------------------------------
+export FB_WIDTH=640
+export FB_HEIGHT=480
+export BKWALL_WIDTH=192
+export BKWALL_HEIGHT=144
+stride_=$(cat /sys/class/graphics/fb0/stride)
+bits_per_pixel_=$(cat /sys/class/graphics/fb0/bits_per_pixel)
+virtual_size=$(cat /sys/class/graphics/fb0/virtual_size)
+tmp1=$[ $bits_per_pixel_ / 8 ]
+export real_width=$[ $stride_ / $tmp1 ]
+##### -------------------------------------
+
+##### -------------------------------------
+# pick first available framebuffer device
+# change for your needs here!
+export fb_device=$(ls -1 /dev/fb*|tail -1)
+##### -------------------------------------
+
