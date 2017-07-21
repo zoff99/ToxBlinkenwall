c6fbfe3f29786ca28999ec3038f3a3e7c6649044
Zoff <zoff99@users.noreply.github.com>
2017-07-21 16:17:28 +0200

    Update show_loading_endless_in_bg.sh

--- toxblinkenwall/scripts/show_loading_endless_in_bg.sh
+++ toxblinkenwall/scripts/show_loading_endless_in_bg.sh
@@ -9,24 +9,7 @@ touchfile=~/ToxBlinkenwall/toxblinkenwall/_endless_loading_stop.txt
 
 rm -f "$touchfile"
 
-##### -------------------------------------
-export FB_WIDTH=640
-export FB_HEIGHT=480
-export BKWALL_WIDTH=192
-export BKWALL_HEIGHT=144
-stride_=$(cat /sys/class/graphics/fb0/stride)
-bits_per_pixel_=$(cat /sys/class/graphics/fb0/bits_per_pixel)
-virtual_size=$(cat /sys/class/graphics/fb0/virtual_size)
-tmp1=$[ $bits_per_pixel_ / 8 ]
-export real_width=$[ $stride_ / $tmp1 ]
-##### -------------------------------------
-
-##### -------------------------------------
-# pick first available framebuffer device
-# change for your needs here!
-export fb_device=$(ls -1 /dev/fb*|tail -1)
-##### -------------------------------------
-
+. $(dirname "$0")/vars.sh
 
 img_0="$gfx_dir""/""loading_bar_0.rgba"
 img_1="$gfx_dir""/""loading_bar_25.rgba"
