ad71f38f6d62cc2d7c7502a2d1a07e94dc988425
Zoff <zoff99@users.noreply.github.com>
2017-07-21 16:20:29 +0200

    Update vars.sh

--- toxblinkenwall/scripts/vars.sh
+++ toxblinkenwall/scripts/vars.sh
@@ -3,8 +3,15 @@
 ##### -------------------------------------
 export FB_WIDTH=640
 export FB_HEIGHT=480
-export BKWALL_WIDTH=192
-export BKWALL_HEIGHT=144
+
+if [ "$IS_ON""x" == "RASPI""x" ]; then
+  export BKWALL_WIDTH=640
+  export BKWALL_HEIGHT=480
+else if [ "$IS_ON""x" == "BLINKENWALL""x" ]; then
+  export BKWALL_WIDTH=192
+  export BKWALL_HEIGHT=144
+fi
+
 stride_=$(cat /sys/class/graphics/fb0/stride)
 bits_per_pixel_=$(cat /sys/class/graphics/fb0/bits_per_pixel)
 virtual_size=$(cat /sys/class/graphics/fb0/virtual_size)
