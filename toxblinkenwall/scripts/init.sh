5350dc64876fe390a23aa582039aa515769d3995
Zoff <zoff99@users.noreply.github.com>
2017-07-21 16:16:39 +0200

    Update init.sh

--- toxblinkenwall/scripts/init.sh
+++ toxblinkenwall/scripts/init.sh
@@ -1,37 +1,21 @@
 #! /bin/bash
 
-##### -------------------------------------
-export FB_WIDTH=640
-export FB_HEIGHT=480
-export BKWALL_WIDTH=192
-export BKWALL_HEIGHT=144
-stride_=$(cat /sys/class/graphics/fb0/stride)
-bits_per_pixel_=$(cat /sys/class/graphics/fb0/bits_per_pixel)
-virtual_size=$(cat /sys/class/graphics/fb0/virtual_size)
-tmp1=$[ $bits_per_pixel_ / 8 ]
-export real_width=$[ $stride_ / $tmp1 ]
-##### -------------------------------------
-
-##### -------------------------------------
-# pick first available framebuffer device
-# change for your needs here!
-export fb_device=$(ls -1 /dev/fb*|tail -1)
-##### -------------------------------------
+. $(dirname "$0")/vars.sh
 
 # ---- only for RASPI ----
-sudo /etc/init.d/lightdm stop
+if [ "$IS_ON""x" == "RASPI""x" ]; then
+  sudo /etc/init.d/lightdm stop
+fi
 # ---- only for RASPI ----
 
 ##### --- remove cursor ---
-printf 'setterm --cursor off > /dev/tty0\nexit\n' | sudo bash
-printf 'setterm --cursor off > /dev/tty1\nexit\n' | sudo bash
+if [ "$IS_ON""x" == "RASPI""x" ]; then
+  printf 'setterm --cursor off > /dev/tty0\nexit\n' | sudo bash
+  printf 'setterm --cursor off > /dev/tty1\nexit\n' | sudo bash
+fi
 ##### --- remove cursor ---
 
 ##### --- setup framebuffer ---
-export FB_WIDTH=640
-export FB_HEIGHT=480
-export BKWALL_WIDTH=192
-export BKWALL_HEIGHT=144
 fbset -s
 fbset -fb "$fb_device" -xres ${FB_WIDTH} -yres ${FB_HEIGHT} -vxres ${FB_WIDTH} -vyres ${FB_HEIGHT} -depth 32
 fbset -s
