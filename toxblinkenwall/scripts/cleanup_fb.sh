b458f9ff49ef768f22d39688ae7227f4d60b11bc
Zoff <zoff99@users.noreply.github.com>
2017-07-21 16:19:20 +0200

    Update cleanup_fb.sh

--- toxblinkenwall/scripts/cleanup_fb.sh
+++ toxblinkenwall/scripts/cleanup_fb.sh
@@ -3,15 +3,13 @@
 . $(dirname "$0")/vars.sh
 
 ##### --- remove cursor ---
-printf 'setterm --cursor off > /dev/tty0\nexit\n' | sudo bash
-printf 'setterm --cursor off > /dev/tty1\nexit\n' | sudo bash
+if [ "$IS_ON""x" == "RASPI""x" ]; then
+  printf 'setterm --cursor off > /dev/tty0\nexit\n' | sudo bash
+  printf 'setterm --cursor off > /dev/tty1\nexit\n' | sudo bash
+fi
 ##### --- remove cursor ---
 
 ##### --- setup framebuffer ---
-export FB_WIDTH=640
-export FB_HEIGHT=480
-export BKWALL_WIDTH=192
-export BKWALL_HEIGHT=144
 fbset -s
 fbset -fb "$fb_device" -xres ${FB_WIDTH} -yres ${FB_HEIGHT} -vxres ${FB_WIDTH} -vyres ${FB_HEIGHT} -depth 32
 fbset -s
