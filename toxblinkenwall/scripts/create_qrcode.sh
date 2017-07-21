f91a984f61db032280542b2df89afaebda8f5090
Zoff <zoff99@users.noreply.github.com>
2017-07-21 16:13:48 +0200

    Update create_qrcode.sh

--- toxblinkenwall/scripts/create_qrcode.sh
+++ toxblinkenwall/scripts/create_qrcode.sh
@@ -4,18 +4,7 @@ qrcode_file_png=~/ToxBlinkenwall/toxblinkenwall/toxid.png
 qrcode_file_rgba=~/ToxBlinkenwall/toxblinkenwall/toxid.rgba
 toxidstr="$1"
 
-##### -------------------------------------
-export FB_WIDTH=640
-export FB_HEIGHT=480
-export BKWALL_WIDTH=192
-export BKWALL_HEIGHT=144
-stride_=$(cat /sys/class/graphics/fb0/stride)
-bits_per_pixel_=$(cat /sys/class/graphics/fb0/bits_per_pixel)
-virtual_size=$(cat /sys/class/graphics/fb0/virtual_size)
-tmp1=$[ $bits_per_pixel_ / 8 ]
-export real_width=$[ $stride_ / $tmp1 ]
-##### -------------------------------------
-
+. $(dirname "$0")/vars.sh
 
 rm -f "$qrcode_file_png"
 rm -f "aa""$$"".png"
