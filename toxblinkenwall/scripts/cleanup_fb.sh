08a6925bddc0adab8b22f3282f4ddf90bc7a88fc
Zoff <zoff99@users.noreply.github.com>
2017-07-21 16:12:56 +0200

    Update cleanup_fb.sh

--- toxblinkenwall/scripts/cleanup_fb.sh
+++ toxblinkenwall/scripts/cleanup_fb.sh
@@ -1,22 +1,6 @@
 #! /bin/bash
 
-##### -------------------------------------
-export FB_WIDTH=640
-export FB_HEIGHT=480
-export BKWALL_WIDTH=192
-export BKWALL_HEIGHT=144
-stride_=$(cat /sys/class/graphics/fb0/stride)
-bits_per_pixel_=$(cat /sys/class/graphics/fb0/bits_per_pixel)
-virtual_size=$(cat /sys/class/graphics/fb0/virtual_size)
-tmp1=$[ $bits_per_pixel_ / 8 ]
-export real_width=$[ $stride_ / $tmp1 ]
-##### -------------------------------------
-
-##### -------------------------------------
-# pick first available framebuffer device
-# change for your needs here!
-export fb_device=$(ls -1 /dev/fb*|tail -1)
-##### -------------------------------------
+. $(dirname "$0")/vars.sh
 
 ##### --- remove cursor ---
 printf 'setterm --cursor off > /dev/tty0\nexit\n' | sudo bash
