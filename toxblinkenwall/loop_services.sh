4a06999723c171e6340c69db42edf945a2425744
Zoff <zoff99@users.noreply.github.com>
2017-07-22 08:57:58 +0200

    vars

--- toxblinkenwall/loop_services.sh
+++ toxblinkenwall/loop_services.sh
@@ -23,22 +23,17 @@ trap clean_up SIGHUP SIGINT SIGTERM
 chmod u+x scripts/*.sh
 chmod u+x toxblinkenwall
 scripts/stop_loading_endless.sh
+scripts/stop_image_endless.sh
 scripts/init.sh
 sleep 2
 scripts/create_gfx.sh
 
 while [ 1 == 1 ]; do
-	#####################################################
-	# pick first available video device
-	# change for your needs here!
-	video_device=$(ls -1 /dev/video*|tail -1)
-	#
-	#####################################################
-
 	scripts/stop_loading_endless.sh
+	scripts/stop_image_endless.sh
 	scripts/init.sh
 	. scripts/vars.sh
-	./toxblinkenwall -d "$video_device" -j "$BKWALL_WIDTH" -k "$BKWALL_HEIGHT"
+	./toxblinkenwall -d "$video_device" -u "$fb_device" -j "$BKWALL_WIDTH" -k "$BKWALL_HEIGHT"
 	sleep 10
 done
 
