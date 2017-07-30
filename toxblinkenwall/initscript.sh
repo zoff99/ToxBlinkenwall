1d614280ebdb3aeb95c0bff98b5e1a7bdbfded03
Zoff <zoff99@users.noreply.github.com>
2017-07-30 18:23:34 +0200

    Create initscript.sh

---
+++ toxblinkenwall/initscript.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+cmd_grep='loop_services.sh'
+cmd_grep2='toxblinkenwall'
+
+procs="$(pgrep -f "$cmd_grep")"
+
+command="$1"
+
+if [ "$command""x" == "startx" ]; then
+	if [ "$procs""x" == "x" ];then
+		cd ~/ToxBlinkenwall/toxblinkenwall/
+		./loop_services.sh > /dev/null 2>/dev/null &
+	fi
+	echo "running"
+else
+	for p in $(pgrep -f "$cmd_grep");do
+		echo kill $p
+		kill $p
+	done
+
+	sleep 10
+	for p in $(pgrep -f "$cmd_grep2");do
+		echo kill $p
+		kill $p
+	done
+	echo "stopped"
+fi
