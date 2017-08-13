43a65f90784eef575fd14004565f138ef05c26f7
Zoff <zoff99@users.noreply.github.com>
2017-08-13 17:46:03 +0200

    create qrcode in bg

--- toxblinkenwall/scripts/create_qrcode.sh
+++ toxblinkenwall/scripts/create_qrcode.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+function background
+{
+
 qrcode_file_png=~/ToxBlinkenwall/toxblinkenwall/toxid.png
 qrcode_file_rgba=~/ToxBlinkenwall/toxblinkenwall/toxid.rgba
 toxidstr="$1"
@@ -13,3 +16,8 @@ convert "$qrcode_file_png" -scale "${BKWALL_WIDTH}x${BKWALL_HEIGHT}" "aa""$$"".p
 rm -f "$qrcode_file_rgba"
 convert "aa""$$"".png" -gravity northwest -background black -extent "${real_width}x${FB_HEIGHT}" "$qrcode_file_rgba"
 rm -f "aa""$$"".png"
+
+}
+
+background "$1" &
+
