2da1714ed64996884f69b7fadcb8efcb9c1fd6cc
zoff99 <you@example.com>
2017-08-19 14:35:59 +0200

    script

--- toxblinkenwall/update_compile.sh
+++ toxblinkenwall/update_compile.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-gcc -O2 -fPIC -export-dynamic \
--I~/inst/include -o toxblinkenwall -lm toxblinkenwall.c -std=gnu99 \
--L~/inst/lib ~/inst/lib/libtoxcore.a ~/inst/lib/libtoxav.a \
--lrt ~/inst/lib/libopus.a -lvpx -lm ~/inst/lib/libsodium.a \
+
+pushd ~
+export _INST_="$(pwd)""/inst/"
+popd
+
+gcc -O2 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall \
+-lm toxblinkenwall.c -std=gnu99 -L$_INST_/lib \
+$_INST_/lib/libtoxcore.a $_INST_/lib/libtoxav.a \
+-lrt $_INST_/lib/libopus.a -lvpx -lm $_INST_/lib/libsodium.a \
 -lao -lpthread -lv4lconvert
