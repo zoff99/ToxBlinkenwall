578c3f15d8e5489979d45fbade6329dad45398fc
Zoff <zoff99@users.noreply.github.com>
2017-10-07 15:49:44 +0200

    Update update_compile.sh

--- toxblinkenwall/update_compile.sh
+++ toxblinkenwall/update_compile.sh
@@ -4,8 +4,11 @@ pushd ~
 export _INST_="$(pwd)""/inst/"
 popd
 
-gcc -O2 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall \
--lm toxblinkenwall.c -std=gnu99 -L$_INST_/lib \
-$_INST_/lib/libtoxcore.a $_INST_/lib/libtoxav.a \
--lrt $_INST_/lib/libopus.a -lvpx -lm $_INST_/lib/libsodium.a \
+gcc -O2 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall -lm toxblinkenwall.c \
+-std=gnu99 \
+-L$_INST_/lib \
+$_INST_/lib/libtoxcore.a \
+$_INST_/lib/libtoxav.a \
+-lrt $_INST_/lib/libopus.a \
+-lvpx -lm $_INST_/lib/libsodium.a \
 -lao -lpthread -lv4lconvert
