79a8c4bb29f8364d0040c9c52d1a34243f7dbaae
Zoff <zoff99@users.noreply.github.com>
2017-10-14 20:54:34 +0200

    update dbg function (add timestamp)

--- toxblinkenwall/toxblinkenwall.c
+++ toxblinkenwall/toxblinkenwall.c
@@ -374,72 +374,90 @@ FriendsList Friends;
 
 void dbg(int level, const char *fmt, ...)
 {
-	char *level_and_format = NULL;
-	char *fmt_copy = NULL;
+    char *level_and_format = NULL;
+    char *fmt_copy = NULL;
 
-	if (fmt == NULL)
-	{
-		return;
-	}
+    if (fmt == NULL)
+    {
+        return;
+    }
 
-	if (strlen(fmt) < 1)
-	{
-		return;
-	}
+    if (strlen(fmt) < 1)
+    {
+        return;
+    }
 
-	if (!logfile)
-	{
-		return;
-	}
+    if (!logfile)
+    {
+        return;
+    }
 
-	if ((level < 0) || (level > 9))
-	{
-		level = 0;
-	}
+    if ((level < 0) || (level > 9))
+    {
+        level = 0;
+    }
 
-	level_and_format = malloc(strlen(fmt) + 3);
+    level_and_format = malloc(strlen(fmt) + 3 + 1);
 
-	if (!level_and_format)
-	{
-		// fprintf(stderr, "free:000a\n");
-		return;
-	}
+    if (!level_and_format)
+    {
+        // fprintf(stderr, "free:000a\n");
+        return;
+    }
 
-	fmt_copy = level_and_format + 2;
-	strcpy(fmt_copy, fmt);
-	level_and_format[1] = ':';
-	if (level == 0)
-	{
-		level_and_format[0] = 'E';
-	}
-	else if (level == 1)
-	{
-		level_and_format[0] = 'W';
-	}
-	else if (level == 2)
-	{
-		level_and_format[0] = 'I';
-	}
-	else
-	{
-		level_and_format[0] = 'D';
-	}
+    fmt_copy = level_and_format + 2;
+    strcpy(fmt_copy, fmt);
+    level_and_format[1] = ':';
+    if (level == 0)
+    {
+        level_and_format[0] = 'E';
+    }
+    else if (level == 1)
+    {
+        level_and_format[0] = 'W';
+    }
+    else if (level == 2)
+    {
+        level_and_format[0] = 'I';
+    }
+    else
+    {
+        level_and_format[0] = 'D';
+    }
 
-	if (level <= CURRENT_LOG_LEVEL)
-	{
-		va_list ap;
-		va_start(ap, fmt);
-		vfprintf(logfile, level_and_format, ap);
-		va_end(ap);
-	}
+    level_and_format[(strlen(fmt) + 2)] = ' ';
+    level_and_format[(strlen(fmt) + 3)] = '\0';
 
-	// fprintf(stderr, "free:001\n");
-	if (level_and_format)
-	{
-		// fprintf(stderr, "free:001.a\n");
-		free(level_and_format);
-	}
-	// fprintf(stderr, "free:002\n");
+    time_t t3 = time(NULL);
+    struct tm tm3 = *localtime(&t3);
+
+    char *level_and_format_2 = malloc(strlen(level_and_format) + 5 + 3 + 3 + 1 + 3 + 3 + 3 + 1);
+    level_and_format_2[0] = '\0';
+    snprintf(level_and_format_2, (strlen(level_and_format) + 5 + 3 + 3 + 1 + 3 + 3 + 3 + 1),
+             "%04d-%02d-%02d %02d:%02d:%02d:%s",
+             tm3.tm_year + 1900, tm3.tm_mon + 1, tm3.tm_mday,
+             tm3.tm_hour, tm3.tm_min, tm3.tm_sec, level_and_format);
+
+    if (level <= CURRENT_LOG_LEVEL)
+    {
+        va_list ap;
+        va_start(ap, fmt);
+        vfprintf(logfile, level_and_format_2, ap);
+        va_end(ap);
+    }
+
+    // fprintf(stderr, "free:001\n");
+    if (level_and_format)
+    {
+        // fprintf(stderr, "free:001.a\n");
+        free(level_and_format);
+    }
+
+    if (level_and_format_2)
+    {
+        free(level_and_format_2);
+    }
+    // fprintf(stderr, "free:002\n");
 }
 
 
