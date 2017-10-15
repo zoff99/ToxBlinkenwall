8f2f93405fb72530954e45ad0799e8933a0e6c1a
Zoff <zoff99@users.noreply.github.com>
2017-10-15 15:48:01 +0200

    gcc flags

--- toxblinkenwall/update_compile.sh
+++ toxblinkenwall/update_compile.sh
@@ -4,7 +4,7 @@ pushd ~
 export _INST_="$(pwd)""/inst/"
 popd
 
-gcc -O2 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall -lm toxblinkenwall.c \
+gcc -g -O3 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall -lm toxblinkenwall.c \
 -std=gnu99 \
 -L$_INST_/lib \
 $_INST_/lib/libtoxcore.a \
