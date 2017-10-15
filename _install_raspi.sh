709f8a21ca940108edc1b5f821a324962b6c9535
Zoff <zoff99@users.noreply.github.com>
2017-10-15 15:46:02 +0200

    gcc flags

--- _install_raspi.sh
+++ _install_raspi.sh
@@ -94,7 +94,7 @@ make install
 
 cd $_HOME_/ToxBlinkenwall/toxblinkenwall/
 
-gcc -O2 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall -lm toxblinkenwall.c \
+gcc -g -O3 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall -lm toxblinkenwall.c \
 -std=gnu99 \
 -L$_INST_/lib \
 $_INST_/lib/libtoxcore.a \
