124925bdb3d63c12ce3b6401a1b7a6d0dd6758fe
Zoff <zoff99@users.noreply.github.com>
2017-10-19 18:48:28 +0200

    WIP: tweaks

--- toxblinkenwall/update_compile.sh
+++ toxblinkenwall/update_compile.sh
@@ -4,6 +4,9 @@ pushd ~
 export _INST_="$(pwd)""/inst/"
 popd
 
+
+# -lao \
+
 gcc -g -O3 -fPIC -export-dynamic -I$_INST_/include -o toxblinkenwall -lm toxblinkenwall.c \
 -std=gnu99 \
 -L$_INST_/lib \
@@ -11,5 +14,5 @@ $_INST_/lib/libtoxcore.a \
 $_INST_/lib/libtoxav.a \
 -lrt $_INST_/lib/libopus.a \
 -lvpx -lm $_INST_/lib/libsodium.a \
-/usr/lib/arm-linux-gnueabihf/libportaudio.a \
--lao -lpthread -lv4lconvert
+-lportaudio \
+-lpthread -lv4lconvert
