15a5a48c62cb1526ec2fd2c11601f9a2ca0d7319
Zoff <zoff99@users.noreply.github.com>
2017-10-24 14:25:35 +0200

    fix empty else

--- toxblinkenwall/scripts/show_text_as_image_stop.sh
+++ toxblinkenwall/scripts/show_text_as_image_stop.sh
@@ -3,9 +3,8 @@
 if [ "$IS_ON""x" == "RASPI""x" ]; then
     :
 else
-
+	:
 # stop openGL text stuff -----
 # node /usr/local/blinkenwall/poetry/restart.js # bug
 # stop openGL text stuff -----
-
 fi
