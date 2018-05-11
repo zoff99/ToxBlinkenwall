4a30387c252892d1dca4e4d9ccc5b73126af739e
zoff99 <zoff@zoff.cc>
2018-05-11 20:32:16 +0200

    tweak udev script to configure WLAN via usb thumbdrive, save some config files for later use

--- toxblinkenwall/udev.sh
+++ toxblinkenwall/udev.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
+#*********************************
+#
+# ToxBlinkenwall - udev script - part 1
+# (C)Zoff in 2017
+#
+# https://github.com/zoff99/ToxBlinkenwall
+#
+#*********************************
+
 ####################################
 #
 #  create the script:
 #  /etc/udev/rules.d/80-plug-usb-device.rules
 #
-# KERNEL=="sd*[!0-9]", ACTION=="add", SUBSYSTEMS=="usb", RUN+="/home/pi/ToxBlinkenwall/toxblinkenwall/udev.sh '%E{DEVNAME}'"
-#
+# KERNEL=="sd*[!0-9]", ACTION=="add", RUN+="/home/pi/ToxBlinkenwall/toxblinkenwall/udev.sh '%E{DEVNAME}'"
 # as root
 #
 ###################################
