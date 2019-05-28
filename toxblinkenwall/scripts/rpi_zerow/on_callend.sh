20ad2ebb8e4c0625a23e96b076712bbe5ad81709
zoff99 <zoff@zoff.cc>
2019-05-28 21:47:59 +0200

    fix scripts

--- toxblinkenwall/scripts/rpi_zerow/on_callend.sh
+++ toxblinkenwall/scripts/rpi_zerow/on_callend.sh
@@ -2,3 +2,5 @@
 
 # turn red led off
 echo 'default-on' | sudo tee --append /sys/class/leds/led1/trigger
+
+echo "CALL ended" > /home/pi/ToxBlinkenwall/toxblinkenwall/share/online_status.txt
--- toxblinkenwall/scripts/rpi_zerow/on_callstart.sh
+++ toxblinkenwall/scripts/rpi_zerow/on_callstart.sh
@@ -2,3 +2,5 @@
 
 # turn red led on
 echo 'none' | sudo tee --append /sys/class/leds/led1/trigger
+
+echo "in CALL" > /home/pi/ToxBlinkenwall/toxblinkenwall/share/online_status.txt
