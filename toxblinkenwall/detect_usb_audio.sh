ff1d96e54238cd8a53c41d9093c03e4df1b953a0
zoff99 <zoff@zoff.cc>
2019-06-04 20:32:24 +0200

    fix display errors on tbw start when also starting detect audio script

--- toxblinkenwall/detect_usb_audio.sh
+++ toxblinkenwall/detect_usb_audio.sh
@@ -42,7 +42,7 @@ echo "... READY" >> "$logfile" 2>&1
 detected_supported_device=0
 
 # detect Sony Playstation Eye ------------
-cat /proc/asound/card*/usbid |grep '1415:2000'
+cat /proc/asound/card*/usbid |grep '1415:2000' >/dev/null 2>/dev/null
 res=$?
 if [ $res -eq 0 ]; then
     echo "Sony Playstation Eye detected ..." >> "$logfile" 2>&1
@@ -136,7 +136,7 @@ fi
 
 
 # detect Acusis v1.1.2.1 (UAC 1.0) ----------------
-cat /proc/asound/card*/usbid |grep '20b1:0011'
+cat /proc/asound/card*/usbid |grep '20b1:0011' >/dev/null 2>/dev/null
 res=$?
 if [ $res -eq 0 ]; then
 	echo "Acusis detected ..." >> "$logfile" 2>&1
@@ -182,7 +182,7 @@ fi
 
 
 # detect USB PhoneReceiver ----------------
-cat /proc/asound/card*/usbid |grep '04b4:0306'
+cat /proc/asound/card*/usbid |grep '04b4:0306' >/dev/null 2>/dev/null
 res=$?
 if [ $res -eq 0 ]; then
     echo "USB PhoneReceiver detected ..." >> "$logfile" 2>&1
