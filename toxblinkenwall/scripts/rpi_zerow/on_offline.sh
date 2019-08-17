61e38bfe54c3097d3ac336408e41379bfb1dd142
zoff99 <zoff@zoff.cc>
2019-08-17 10:08:41 +0200

    remove trainling spaces

--- toxblinkenwall/scripts/rpi_zerow/on_offline.sh
+++ toxblinkenwall/scripts/rpi_zerow/on_offline.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # turn green led off, when offline
-echo 'default-on' | sudo tee --append /sys/class/leds/led0/trigger 
+echo 'default-on' | sudo tee --append /sys/class/leds/led0/trigger
 
 echo "*offline*" > /home/pi/ToxBlinkenwall/toxblinkenwall/share/online_status.txt
--- toxblinkenwall/scripts/rpi_zerow/on_online.sh
+++ toxblinkenwall/scripts/rpi_zerow/on_online.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # turn green led on, when online
-echo 'none' | sudo tee --append /sys/class/leds/led0/trigger 
+echo 'none' | sudo tee --append /sys/class/leds/led0/trigger
 
 echo "ONLINE" > /home/pi/ToxBlinkenwall/toxblinkenwall/share/online_status.txt
