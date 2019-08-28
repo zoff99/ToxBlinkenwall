5857d2647b57f8b3dcc59f1bbdb0ff6e6f6476de
zoff99 <zoff@zoff.cc>
2019-08-28 19:25:11 +0200

    add license text

--- toxblinkenwall/toxblinkenwall.c
+++ toxblinkenwall/toxblinkenwall.c
@@ -8,6 +8,24 @@
  * dirty hack (echobot and toxic were used as blueprint)
  *
  */
+/*
+ * Copyright © 2017 - 2019 Zoff <zoff@zoff.cc>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ *
+ */
 
 // network.c:498:sendpacket:attempted to send message with network family 10 (probably IPv6) on IPv4 socket
 
