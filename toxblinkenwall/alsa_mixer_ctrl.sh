e43faaf555579ffd2c51dd41dae238e6889360f6
Jonathan Sieber <mail@strfry.org>
2019-11-19 00:02:26 +0100

    Simplify alsa_mixer_ctrl.sh to rely on PAs virtual Master

--- toxblinkenwall/alsa_mixer_ctrl.sh
+++ toxblinkenwall/alsa_mixer_ctrl.sh
@@ -9,41 +9,17 @@ else
     want_up=1
 fi
 
-mapped_volume=" -M "
-
-current_value_percent=0
-
-for device_num in 0 1; do
-    # loop through all playback volumes
-    amixer -c"$device_num" scontrols 2>&1|head -3|grep -i 'invalid card number' >/dev/null 2>&1
-    device_valid=$?
-    if [ $device_valid -eq 1 ]; then
-        for ctrl in $(amixer -c"$device_num" scontrols |sed -e 's#Simple mixer control ##'); do
-            # echo $ctrl
-            amixer -c"$device_num" sget "$ctrl"|grep -i 'pvolume' >/dev/null 2>&1
-            is_playback=$?
-            # echo $is_playback
-            if [ $is_playback -eq 0 ]; then
-                if [ "$want_up""x" == "2x" ]; then
-                    :
-                    # just return current value, without setting anything
-                else
-                    if [ "$want_up""x" == "1x" ]; then
-                        amixer $mapped_volume -c"$device_num" sset "$ctrl" "3dB+" unmute >/dev/null 2>&1
-                    else
-                        amixer $mapped_volume -c"$device_num" sset "$ctrl" "3dB-" unmute >/dev/null 2>&1
-                    fi
-                fi
-            fi
-        done
-        current_value_percent_=$(amixer $mapped_volume -c"$device_num"|grep -i "playback"|grep '%'|grep -o '[^\[]*%'|sed -e 's#%##'|tr -d ' ')
-        current_value_percent_=$[ $[ $current_value_percent_ + 1 ] - 1 ]
-        # echo $current_value_percent_
-        if [ $current_value_percent_ -le 100 ] && [ $current_value_percent_ -ge 0 ]; then
-            current_value_percent=$current_value_percent_
-        fi
-
+# loop through all playback volumes
+if [ "$want_up""x" == "2x" ]; then
+    :
+# just return current value, without setting anything
+else
+    if [ "$want_up""x" == "1x" ]; then
+        amixer set Master 3dB+
+    else
+        amixer set Master 3dB-
     fi
-done
+fi
 
+current_value_percent=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master) | sed -e s/%//)
 echo "CURVALUE:""$current_value_percent"
