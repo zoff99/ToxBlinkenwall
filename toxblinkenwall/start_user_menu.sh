6c30ec0ee2016cbc83ec8d96e3237edfedef36e6
zoff99 <zoff@zoff.cc>
2019-12-13 20:35:40 +0100

    add delay

--- toxblinkenwall/start_user_menu.sh
+++ toxblinkenwall/start_user_menu.sh
@@ -9,4 +9,5 @@
 #
 #*********************************
 
+sleep 18 # wait for tbw to shutdown properly
 sudo openvt -c 1 -fs -- su - pi bash -c /home/pi/ToxBlinkenwall/toxblinkenwall/scripts/user_menu.sh &
