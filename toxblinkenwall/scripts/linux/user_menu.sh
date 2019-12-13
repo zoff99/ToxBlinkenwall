670e47fe12a125f70f2a1e230e41e40e83a744c0
zoff99 <zoff@zoff.cc>
2019-12-13 21:07:31 +0100

    loop menu on some entries

--- toxblinkenwall/scripts/linux/user_menu.sh
+++ toxblinkenwall/scripts/linux/user_menu.sh
@@ -1,23 +1,5 @@
 #! /bin/bash
 
-CHOICE=$(dialog --cancel-label "Start" \
---default-item "Add" \
---menu "ToxBlinkenwall - Main Menu" \
-0 0 0 \
-Start "ToxBlinkenwall" \
-Add "a friend by ToxID" \
-Shutdown "System" \
-Exit "" \
-3>&1 1>&2 2>&3 )
-
-res=$?
-clear
-
-# Return status of non-zero indicates cancel
-if [ $res != 0 ]; then
-    CHOICE="Start"
-fi
-
 function enter_qrcode()
 {
     echo "################################"
@@ -187,26 +169,49 @@ function func_exit() {
     exit
 }
 
-case $CHOICE in
-    Start)
-        # echo "start ....."
-        func_start
-        ;;
-    Add)
-        # echo "phonebook_menu"
-        func_addfriend
-        ;;
-    Shutdown)
-        # echo "shutdown"
-        func_shutdown
-        ;;
-    Exit)
-        # echo "exit"
-        func_exit
-        ;;
-    *)
-        # echo "++default++"
-        func_exit
-        ;;
-esac
+while : ; do
 
+    CHOICE=$(dialog --cancel-label "Start" \
+    --default-item "Add" \
+    --menu "ToxBlinkenwall - Main Menu" \
+    0 0 0 \
+    Start "ToxBlinkenwall" \
+    Add "a friend by ToxID" \
+    Shutdown "System" \
+    Exit "" \
+    3>&1 1>&2 2>&3 )
+
+    res=$?
+    clear
+
+    # Return status of non-zero indicates cancel
+    if [ $res != 0 ]; then
+        CHOICE="Start"
+    fi
+
+
+    case $CHOICE in
+        Start)
+            # echo "start ....."
+            func_start
+            ;;
+        Add)
+            # echo "phonebook_menu"
+            func_addfriend
+            # --> this will continue to the main menu
+            ;;
+        Shutdown)
+            # echo "shutdown"
+            func_shutdown
+            ;;
+        Exit)
+            # echo "exit"
+            func_exit
+            ;;
+        *)
+            # echo "++default++"
+            func_exit
+            ;;
+    esac
+
+done
